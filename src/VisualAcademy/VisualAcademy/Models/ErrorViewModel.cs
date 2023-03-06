31c3fe4fde7dbec5df1e165358c670d07937fbe8
Park YongJune <redplus@redplus.net>
2023-03-06 10:07:08 +0900

    VisualAcademy Project Create

---
+++ src/VisualAcademy/VisualAcademy/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace VisualAcademy.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

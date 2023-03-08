ab73500548f15a9a412a95f6dd705e9d02f66ddd
Park YongJune <redplus@redplus.net>
2023-03-08 11:13:19 +0900

    .

--- src/VisualAcademy/VisualAcademy/Data/ApplicationDbContext.cs
+++ src/VisualAcademy/VisualAcademy/Data/ApplicationDbContext.cs
@@ -3,11 +3,15 @@ using Microsoft.EntityFrameworkCore;
 using VisualAcademy.Models;
 
 namespace VisualAcademy.Data {
+    // ApplicationDbContext 클래스 생성 구문
+    // 이 클래스는 EF Core DbContext 클래스를 상속하여, 
+    // 데이터베이스 연결과 AppointmentsTypes 테이블을 매핑한다.
     public class ApplicationDbContext : IdentityDbContext {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options) {
         }
 
+        // AppointmentTypes 프로퍼티는 AppointmentsTypes 테이블과 매핑된 DbSet이다.
         public DbSet<AppointmentType> AppointmentTypes { get; set; }
     }
 }

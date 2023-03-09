00fe8629d8599efc7ae6c53bababd5b18b997e9b
Park YongJune <redplus@redplus.net>
2023-03-09 18:26:41 +0900

    비동기 메서드 추가

---
+++ src/VisualAcademy/VisualAcademy/Repositories/AppointmentTypeRepositoryAsync.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using VisualAcademy.Data;
+using VisualAcademy.Models;
+
+namespace VisualAcademy.Repositories {
+    public class AppointmentTypeRepositoryAsync : IAppointmentTypeRepositoryAsync {
+        private readonly ApplicationDbContext _context;
+
+        public AppointmentTypeRepositoryAsync(ApplicationDbContext context) {
+            _context = context;
+        }
+
+        public async Task AddAppointmentType(AppointmentType appointmentType) {
+            await _context.AppointmentTypes.AddAsync(appointmentType);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAppointmentType(int id) {
+            var appointmentType = await _context.AppointmentTypes.FindAsync(id);
+            _context.AppointmentTypes.Remove(appointmentType);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<AppointmentType> GetAppointmentType(int id) {
+            return await _context.AppointmentTypes.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<AppointmentType>> GetAppointmentTypes() {
+            return await _context.AppointmentTypes.ToListAsync();
+        }
+
+        public async Task UpdateAppointmentType(AppointmentType appointmentType) {
+            _context.Update(appointmentType);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ src/VisualAcademy/VisualAcademy/Repositories/IAppointmentTypeRepositoryAsync.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using VisualAcademy.Models;
+
+namespace VisualAcademy.Repositories {
+    public interface IAppointmentTypeRepositoryAsync {
+        Task<IEnumerable<AppointmentType>> GetAppointmentTypes();
+        Task<AppointmentType> GetAppointmentType(int id);
+        Task AddAppointmentType(AppointmentType appointmentType);
+        Task UpdateAppointmentType(AppointmentType appointmentType);
+        Task DeleteAppointmentType(int id);
+    }
+}
